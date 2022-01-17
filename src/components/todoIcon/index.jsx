f2848a6aa911d027e64c22bba42b2a9b4f568b97
coder-master <nikiforof.fri@yandex.ru>
2022-01-18 02:17:54 +0300

    Component refactoring (TodoIcon)

--- src/components/todoIcon/index.jsx
+++ src/components/todoIcon/index.jsx
@@ -4,11 +4,31 @@ import styles from './index.module.scss';
 
 const TodoIcon = ({ icon, large, disabled, badge }) => {
     return (
-        <div className={styles.root}>
-            <Icon
-                className={`${styles.icon} ${large ? styles.large : ''}
-                ${disabled ? styles.disabled : ''}`}
-                icon={icon} />
+        <div
+            className={`${styles.root} ${large ? styles.large : ''} ${disabled ? styles.disabled : ''
+                }`}>
+            <svg
+                className={styles.circle}
+                viewBox='0 0 60 60'
+                xmlns='http://www.w3.org/2000/svg'>
+                <g>
+                    <circle
+                        className={styles.background}
+                        r='48%'
+                        cy='50%'
+                        cx='50%'
+                        strokeLinecap='round'
+                    />
+                    <circle
+                        className={styles.foreground}
+                        r='48%'
+                        cy='50%'
+                        cx='50%'
+                        strokeLinecap='round'
+                    />
+                </g>
+            </svg>
+            <Icon icon={icon} className={styles.icon} />
             {badge && <div className={styles.badge}>{badge}</div>}
         </div>
     )
